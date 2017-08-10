586946cebb14c2fb052ac47bfc25414662bb726f
dhanukaperera <dhanukashehanperera@gmail.com>
2017-08-10 08:28:01 +0530

    digital clock component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DigitalClockComponent } from './components/digital-clock/digital-clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigitalClockComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/digital-clock/digital-clock.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { DigitalClockComponent } from './digital-clock.component';
+
+describe('DigitalClockComponent', () => {
+  let component: DigitalClockComponent;
+  let fixture: ComponentFixture<DigitalClockComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DigitalClockComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DigitalClockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
